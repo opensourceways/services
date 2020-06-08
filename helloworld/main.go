540196ee4158ae25bb82cd3125dbdd68548ba81e
Asim Aslam <asim@aslam.me>
2020-06-08 10:26:27 +0100

    Update helloworld service

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"context"
-	"log"
-
-	pb "github.com/micro/examples/helloworld/proto"
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/util/log"
+	"github.com/micro/services/helloworld/handler"
+
+	helloworld "github.com/micro/services/helloworld/proto"
 )
 
-type Greeter struct{}
-
-func (g *Greeter) Hello(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
-	rsp.Greeting = "Hello " + req.Name
-	return nil
-}
-
 func main() {
+	// New Service
 	service := micro.NewService(
-		micro.Name("helloworld"),
+		micro.Name("go.micro.service.helloworld"),
+		micro.Version("latest"),
 	)
 
+	// Initialise service
 	service.Init()
 
-	pb.RegisterGreeterHandler(service.Server(), new(Greeter))
+	// Register Handler
+	helloworld.RegisterHelloworldHandler(service.Server(), new(handler.Helloworld))
 
+	// Run service
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}

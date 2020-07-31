40e50aecc50265da724cc9a410ea2bd0b6dc8898
Ben Toogood <bentoogood@gmail.com>
2020-07-31 11:03:24 +0100

    Update logger

--- helloworld/handler/helloworld.go
+++ helloworld/handler/helloworld.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"context"
 
-	"github.com/micro/go-micro/v3/util/log"
+	"github.com/micro/micro/v3/service/logger"
 
 	helloworld "github.com/micro/services/helloworld/proto"
 )
@@ -12,7 +12,7 @@ type Helloworld struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Helloworld) Call(ctx context.Context, req *helloworld.Request, rsp *helloworld.Response) error {
-	log.Log("Received Helloworld.Call request")
+	logger.Info("Received Helloworld.Call request")
 	rsp.Msg = "Hello " + req.Name
 	return nil
 }

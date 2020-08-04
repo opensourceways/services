10074ebd4ffe918b6f62fb4123e921ac7e1809d2
Asim Aslam <asim@aslam.me>
2020-08-04 22:25:01 +0100

    reduce helloworld to pb

--- helloworld/handler/helloworld.go
+++ helloworld/handler/helloworld.go
@@ -4,14 +4,13 @@ import (
 	"context"
 
 	"github.com/micro/go-micro/v3/util/log"
-
-	helloworld "github.com/micro/services/helloworld/proto"
+	pb "github.com/micro/services/helloworld/proto"
 )
 
 type Helloworld struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
-func (h *Helloworld) Call(ctx context.Context, req *helloworld.Request, rsp *helloworld.Response) error {
+func (h *Helloworld) Call(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
 	log.Log("Received Helloworld.Call request")
 	rsp.Msg = "Hello " + req.Name
 	return nil

6598f0690cee0151f42622bbc177904ac29d1d5a
Asim Aslam <asim@aslam.me>
2020-08-05 14:33:53 +0100

    Update comments.go

--- blog/comments/handler/comments.go
+++ blog/comments/handler/comments.go
@@ -11,37 +11,7 @@ type Comments struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (c *Comments) Call(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
-	logger.Info("Received Comments.Call request")
-	rsp.Msg = "Hello " + req.Name
+	logger.Info("Not yet implemented")
 	return nil
 }
 
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (c *Comments) Stream(ctx context.Context, req *pb.StreamingRequest, stream pb.Comments_StreamStream) error {
-	logger.Infof("Received Comments.Stream request with count: %d", req.Count)
-
-	for i := 0; i < int(req.Count); i++ {
-		logger.Infof("Responding: %d", i)
-		if err := stream.Send(&pb.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (c *Comments) PingPong(ctx context.Context, stream pb.Comments_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		logger.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&pb.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
-	}
-}
